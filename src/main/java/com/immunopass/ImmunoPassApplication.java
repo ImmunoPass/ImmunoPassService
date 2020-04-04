ee1fc596f54692d1a51df8c6fe4f01e276efde71
Ankur Gupta <ankur@ankurgupta.net>
2020-04-04 19:58:14 +0530

    SpringBoot app first working commit.

---
+++ src/main/java/com/immunopass/ImmunoPassApplication.java
@@ -0,0 +1,13 @@
+package com.immunopass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmunoPassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmunoPassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/immunopass/ImmunoPassApplicationTests.java
@@ -0,0 +1,8 @@
+package com.immunopass;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmunoPassApplicationTests {
+
+}
